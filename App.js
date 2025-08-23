61341eeffe7763937e044fb63aeafda99afc3cf9
Rahul Pant <rahulpant@Rahuls-MacBook-Pro.local>
2025-08-24 00:17:39 +0530

    create react element

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const description = React.createElement("h1", { id: "heading" }, "Hello React");
+const Heading = () => <h1>Learning react...!!!</h1>;
+const Description = () => (
+  <div>
+    <Heading />
+    <h1 id="heading">Namaste React</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(description);
+root.render(<Description />);
