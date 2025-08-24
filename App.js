7c5aa7abe06fd2310d0fb62369fc4780d9908d36
Rahul Pant <rahulpant@Rahuls-MacBook-Pro.local>
2025-08-24 10:36:27 +0530

    children routes

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import ContactUs from "./src/components/ContactUs";
+import About from "./src/components/About";
 
-const Heading = () => <h1>Learning react...!!!</h1>;
+const Heading = () => <h1>Learning React...!!!</h1>;
 const Description = () => (
   <div>
     <Heading />
     <h1 id="heading">Namaste React</h1>
+    <Outlet />
   </div>
 );
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Description />,
+    children: [
+      {
+        path: "/contact",
+        element: <ContactUs />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Description />);
+root.render(<RouterProvider router={router} />);
